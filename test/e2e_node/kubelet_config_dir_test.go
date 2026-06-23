2cb3341c1574fd2c0b9dfb65b35c8e9973934506
Sergey Kanzhelev <S.Kanzhelev@live.com>
2026-06-22 22:17:10 -0700

    remove unnecessary comment

--- test/e2e_node/kubelet_config_dir_test.go
+++ test/e2e_node/kubelet_config_dir_test.go
@@ -198,10 +198,6 @@ eventBurst: 101`)
 
 			dropinConfigPath := filepath.Join(configDir, "10-kubelet.conf")
 			framework.ExpectNoError(os.WriteFile(dropinConfigPath, contents, 0755))
-			// Cleanup of the drop-in file is handled by the Context's AfterEach,
-			// which removes all *.conf files in the drop-in dir. A DeferCleanup
-			// here would collide with that AfterEach and fail trying to remove an
-			// already-deleted file.
 
 			ginkgo.By("Restarting the kubelet")
 			restartKubelet := mustStopKubelet(ctx, f)
